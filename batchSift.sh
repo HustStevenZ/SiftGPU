2ceb4b6a144f1fc12e326e525d06bb5a69af443d
Sanqian Zhao <sanqianzhao@gmail.com>
2016-07-25 17:35:31 +0800

    一个简单的批量脚本

---
+++ batchSift.sh
@@ -0,0 +1,12 @@
+#/usr/bin/env bash
+IMAGE_DIR=$1
+FILES=`ls -S -hr ${IMAGE_DIR}/*.jpg | tail -n 100`
+SIFTCMD="./build/TestWinGlut"
+rm time_consume.csv
+touch time_consume.csv
+for file in ${FILES}
+do
+    SIFTARG_FILE="-i  ${file} -o ${file}.sift"
+    TIME_FILE=$(${SIFTCMD} ${SIFTARG_FILE} | grep "RUN SIFT"| sed -e "s/\[.*\]//")
+    echo ${file} "${TIME_FILE}s">>time_consume.csv
+done
