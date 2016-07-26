3901bd8f2432702f5a33141ff977425c497c2a07
Steven Zhao <xtzsq2004@gmail.com>
2016-07-26 09:48:58 +0800

    让batch脚本显示开始与结束时间

--- batchSift.sh
+++ batchSift.sh
@@ -1,7 +1,9 @@
 #/usr/bin/env bash
 IMAGE_DIR=$1
-FILES=`ls -S -hr ${IMAGE_DIR}/*.jpg | tail -n 100`
+FILES=`ls -S -hr ${IMAGE_DIR}/*.jpg`
 SIFTCMD="./build/TestWinGlut -cuda"
+STARTTIME=$(date +%T)
+echo "Process start at ${STARTTIME}"
 rm time_consume.csv
 touch time_consume.csv
 for file in ${FILES}
@@ -10,4 +12,6 @@ do
     TIME_FILE=$(${SIFTCMD} ${SIFTARG_FILE} | grep "RUN SIFT"| sed -e "s/\[.*\]//")
     echo ${file} "${TIME_FILE}s">>time_consume.csv
 done
+ENDTIME=$(date +%T)
+echo "Process ends at ${ENDTIME}"
 echo "Batch processing is finished, plz backup the time_comsume.csv if it is necessary"
